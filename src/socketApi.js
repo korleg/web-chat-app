b8adbacae3810cf1e0352068c031a9cd9d1c8912
korleg <55947473+korleg@users.noreply.github.com>
2019-11-21 13:15:13 +0300

    socket.io redis adapter

--- src/socketApi.js
+++ src/socketApi.js
@@ -6,6 +6,14 @@ const socketApi = {
     io
 };
 
+//redis adapter
+
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({ 
+    host: process.env.REDIS_URI, 
+    port: process.env.REDIS_PORT 
+}));
+
 io.on('connection', socket =>{
 console.log('User Login');
 });
